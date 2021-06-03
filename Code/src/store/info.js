1e194c9fa60cb15453a6253260534e903b8df107
DanielKrawczyk <80767727+DanielKrawczyk@users.noreply.github.com>
2021-06-03 14:30:04 +0200

    Updates

--- Code/src/store/info.js
+++
@@ -1,8 +0,0 @@
-export default function info(state = '', action) {
-    switch(action.type) {
-        case 'NEW_INFO':
-            return state = action.data;
-        default:
-            return state;
-    }
-}
